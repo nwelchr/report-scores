92ef99b51752b9adf2b8706d9fe6b7ad7da9d9f8
Nick Welch <nwelchr@gmail.com>
2024-06-07 12:54:22 -0700

    Concurrent backend frontend servers

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="App">
+    <div className="bg-red-50">
       <h1>{data.message}</h1>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
-// server/index.js
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
+
+// Use CORS
+app.use(cors());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../my-app/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // An example API endpoint
 app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello from the server!" });
+  res.send({ message: "Hello from the serverr!" });
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../my-app/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
