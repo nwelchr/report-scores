abce72c216cdcaf00099eca867ef97093a1bf5ab
Nick Welch <nwelchr@gmail.com>
2024-06-07 14:47:22 -0700

    Extract entrant

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Entrant from "./components/Entrant";
 
 const fetchEvent = async (slug) => {
   const { data } = await axios.get(`/api/event`, { params: { slug } });
@@ -15,18 +16,6 @@ const extractSlug = (input) => {
     return normalizedSlug;
   }
   return "";
-};
-
-const getColorByPlacement = (placement) => {
-  if (placement === 1) return "bg-emerald-600";
-  if (placement === 2) return "bg-teal-600";
-  if (placement === 3) return "bg-cyan-600";
-  if (placement === 4) return "bg-indigo-600";
-  if (placement === 5) return "bg-violet-600";
-  if (placement === 7) return "bg-rose-600";
-  if (placement >= 9 && placement <= 16) return "bg-red-800";
-  if (placement >= 17 && placement <= 32) return "bg-red-950";
-  return "bg-gray-900";
 };
 
 function App() {
@@ -63,15 +52,7 @@ function App() {
           <h3 className="text-xl mb-2">Standings</h3>
           <ul>
             {data.standings.map((standing) => (
-              <li
-                key={standing.entrant.id}
-                className={`p-2 mb-2 rounded-md ${getColorByPlacement(
-                  standing.placement
-                )}`}
-              >
-                {standing.placement}. {standing.entrant.name} (Seed{" "}
-                {standing.entrant.seeds[0].seedNum})
-              </li>
+              <Entrant standing={standing} />
             ))}
           </ul>
         </div>
