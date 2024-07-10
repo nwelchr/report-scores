29c4617988530bb37c3378c282797015b5a5bf66
Nick Welch <nwelchr@gmail.com>
2024-07-10 15:57:35 -0700

    Get W/L reporting working

--- app/queries/reportSet.js
+++ app/queries/reportSet.js
@@ -1,35 +1,32 @@
 import axios from "axios";
 
 const reportSet = async ({ setId, winnerId, entrantScore, opponentScore }) => {
-  console.log(
-    "WE HIT THISSSSSTHISSSSSTHISSSSSTHISSSSSTHISSSSSTHISSSSSTHISSSSS"
+  const response = await axios.post(
+    "https://api.smash.gg/gql/alpha",
+    {
+      query: `
+        mutation reportSet($setId: ID!, $winnerId: ID!) {
+          reportBracketSet(setId: $setId, winnerId: $winnerId) {
+            id
+            state
+          }
+        }
+      `,
+      variables: {
+        setId,
+        winnerId,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_START_GG_API_TOKEN}`,
+      },
+    }
   );
-  console.log({ setId, winnerId, entrantScore, opponentScore });
-  // const response = await axios.post(
-  //   "https://api.smash.gg/gql/alpha",
-  //   {
-  //     query: `
-  //       mutation reportSet($setId: ID!, $winnerId: ID!, $gameData: [BracketSetGameDataInput]) {
-  //         reportBracketSet(setId: $setId, winnerId: $winnerId, gameData: $gameData) {
-  //           id
-  //           state
-  //         }
-  //       }
-  //     `,
-  //     variables: {
-  //       setId,
-  //       winnerId,
-  //       gameData,
-  //     },
-  //   },
-  //   {
-  //     headers: {
-  //       Authorization: `Bearer ${process.env.NEXT_PUBLIC_START_GG_API_TOKEN}`,
-  //     },
-  //   }
-  // );
 
-  return "Heyooooo";
+  console.log({ response });
+
+  return response;
 };
 
 export default reportSet;
