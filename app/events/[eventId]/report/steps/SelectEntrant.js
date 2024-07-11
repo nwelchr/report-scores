2cc1413cf0666d8b38f940e8ed55c45ff552abc7
Nick Welch <nwelchr@gmail.com>
2024-07-11 14:05:48 -0700

    heyyyy

--- app/events/[eventId]/report/steps/SelectEntrant.js
+++ app/events/[eventId]/report/steps/SelectEntrant.js
@@ -19,16 +19,20 @@ export default function SelectEntrant({ value, entrants, onChange, onSelect }) {
       <div className="fixed inset-0 bg-black bg-opacity-50"></div>
       <div className="fixed inset-0 flex items-center justify-center p-4">
         <DialogPanel className="w-full max-w-md bg-gray-800 p-6 rounded-md shadow-lg text-center">
-          <DialogTitle className="text-4xl mb-4 text-white">Tag</DialogTitle>
+          <DialogTitle className="text-4xl mb-4 text-white">
+            Start.gg Tag
+          </DialogTitle>
           <Description className="mt-2 text-gray-300">
             Please find your tag.
           </Description>
-          <AutocompleteInput
-            value={value}
-            onChange={onChange}
-            onSelect={onSelect}
-            suggestions={entrants || []}
-          />
+          <div className="mt-4 flex justify-center">
+            <AutocompleteInput
+              value={value}
+              onChange={onChange}
+              onSelect={onSelect}
+              suggestions={entrants || []}
+            />
+          </div>
         </DialogPanel>
       </div>
     </Dialog>
