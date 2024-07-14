d0d6325d07a5101394c762fdafc37d1e7e3e9be4
Nick Welch <nwelchr@gmail.com>
2024-07-14 14:10:11 -0700

    Redirect user to start page after submitting

--- app/events/[eventId]/report/submission-confirmation/page.js
+++ app/events/[eventId]/report/submission-confirmation/page.js
@@ -1,9 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
+import { useRouter, useParams } from "next/navigation";
 import SubmissionConfirmation from "./SubmissionConfirmation";
 import ProtectedRoute from "components/ProtectedRoute";
 
 const SubmissionConfirmationPage = () => {
+  const router = useRouter();
+  const { eventId } = useParams();
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      router.push(`/events/${eventId}/report/select-entrant`);
+    }, 2000);
+
+    return () => clearTimeout(timerId);
+  });
   return <SubmissionConfirmation />;
 };
 
